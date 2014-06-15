d70b962845a8f3617a37749d680b5d862268f7ec
rauix <me@rauix.net>
2014-06-15 16:51:58 +0100

    Initial Commit

---
+++ mac2imgur/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  mac2imgur
+//
+//  Created by Miles on 15/06/2014.
+//  Copyright (c) 2014 Miles. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
