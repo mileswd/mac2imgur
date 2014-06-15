392e5c2ca7e66ed6a6bee870e241795d43d69985
Miles <me@mileswd.com>
2014-06-15 16:51:58 +0100

    Initial Commit

---
+++ mac2imgur/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  mac2imgur
+//
+//  Created by Miles on 15/06/2014.
+//  Copyright (c) 2014 Miles. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
