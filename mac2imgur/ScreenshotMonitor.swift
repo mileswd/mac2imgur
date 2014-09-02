c98c1f9372883edb01ebe91e6d72c4bd7e6f2e73
rauix <me@rauix.net>
2014-09-02 20:14:49 +0100

    Minor code tidy up, can't wait for Apple to release the official coding conventions for Swift

--- mac2imgur/ScreenshotMonitor.swift
+++ mac2imgur/ScreenshotMonitor.swift
@@ -1,18 +1,18 @@
 /* This file is part of mac2imgur.
- *
- * mac2imgur is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+*
+* mac2imgur is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
 
- * mac2imgur is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+* mac2imgur is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
 
- * You should have received a copy of the GNU General Public License
- * along with mac2imgur.  If not, see <http://www.gnu.org/licenses/>.
- */
+* You should have received a copy of the GNU General Public License
+* along with mac2imgur.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 import Foundation
 
@@ -45,17 +45,17 @@ class ScreenshotMonitor {
         if isDeletion() || resultCount <= 0 {
             
             // TODO: Make a loop for finding only the item missing and deleting it from blacklist
-            NSLog("Screenshot has been deleted")
-        
+            println("Screenshot has been deleted")
+            
         } else {
             
-            NSLog("RESULTCOUNT: \(resultCount)")
+            println("resultCount =  \(resultCount)")
             
             if resultCount > 0 {
                 
                 var metadataItem: NSMetadataItem = query.resultAtIndex(query.resultCount - 1) as NSMetadataItem
                 
-                NSLog("NSMetadataItem: \(metadataItem.description!)")
+                println("NSMetadataItem: \(metadataItem.description)")
                 
                 // Get the path to the screenshot
                 var screenshotPath: String = metadataItem.valueForKey(NSMetadataItemPathKey) as String
@@ -82,7 +82,7 @@ class ScreenshotMonitor {
                 addToBlacklist(element!.valueForKey(NSMetadataItemPathKey) as String)
             }
         }
-        NSLog("BLACKLIST SIZE \(blacklist.count)")
+        println("Blacklist size = \(blacklist.count)")
     }
     
     func addToBlacklist(screenshotPath: String) {
@@ -93,7 +93,7 @@ class ScreenshotMonitor {
     func removeFromBlackList(screenshotPath: String){
         var indexOfScreenshot: NSInteger?
         
-        for index in 0..<query.resultCount {
+        for (index, element) in enumerate(query.results) {
             if blacklist[index] == screenshotPath {
                 indexOfScreenshot = index
             }
@@ -102,7 +102,7 @@ class ScreenshotMonitor {
         if indexOfScreenshot != nil {
             blacklist.removeAtIndex(indexOfScreenshot!)
         }
-
+        
     }
     
     func isDeletion() -> Bool {
