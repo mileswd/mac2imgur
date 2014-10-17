b9bf2a7ab1e8c2c811725754943c0eceb5c952a9
Miles <me@mileswd.com>
2014-10-18 00:45:29 +0100

    Update to Xcode 6.1 and remove unused import

--- mac2imgur/AppDelegate.swift
+++ mac2imgur/AppDelegate.swift
@@ -14,7 +14,6 @@
 * along with mac2imgur.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-import Foundation
 import Cocoa
 
 class AppDelegate: NSObject, NSApplicationDelegate, NSUserNotificationCenterDelegate {
