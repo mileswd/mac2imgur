846b0443f23e6a5379ec57c6f057343e73d1df78
Miles <me@mileswd.com>
2014-12-10 00:01:14 +0000

    Remove file header

--- mac2imgur/main.swift
+++ mac2imgur/main.swift
@@ -1,11 +1,3 @@
-//
-//  main.swift
-//  mac2imgur
-//
-//  Created by Miles on 15/06/2014.
-//  Copyright (c) 2014 Miles. All rights reserved.
-//
-
 import Cocoa
 
 NSApplicationMain(C_ARGC, C_ARGV)
